52f2dbf1147d896a88d01aee5b72ceedc39ec7c3
kat <kathiacid98@gmail.com>
2025-08-10 20:04:30 -0400

    cositas

--- src/pages/Studies.jsx
+++ src/pages/Studies.jsx
@@ -8,21 +8,21 @@ return (
     <h1>Estudios</h1>
     <Timeline />
     <section className="content">
-        <div className="cursos">
+    <div className="cursos">
         <h2>Cursos</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Molestiae, ducimus aut nemo nihil ab voluptatibus excepturi, illum perspiciatis eaque nulla temporibus tempore incidunt nisi harum odio quidem atque non animi.</p>
-        </div>
-        <img src="./src/assets/imgstudies.png" className="imagen"></img>
-        <div className="softskills">
-            <h2>Softskills</h2>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sint ex, libero quae quas esse odio ducimus deleniti eaque provident cupiditate dignissimos ab error porro magnam blanditiis maxime dolor ipsum?</p>
-        </div>
+    </div>
+    <img src="./src/assets/imgstudies.png" className="imagen" alt="Estudios"></img>
+    <div className="softskills">
+        <h2>Softskills</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos sint ex, libero quae quas esse odio ducimus deleniti eaque provident cupiditate dignissimos ab error porro magnam blanditiis maxime dolor ipsum?</p>
+    </div>
     </section>
     <div className="contact-info">
-        <h3>{t("contact")}</h3>
-        <p>Email: tuemail@mail.com</p>
-        </div>
+    <img src="./src/assets/contactametitulo.png" className="contact-info__image" alt="Contáctame"></img>
+    <img src="./src/assets/flechas.png" className="contact-info__arrows" alt="Flechas"></img>
+    </div>
+    <p>Email: tuemail@mail.com</p>
 </section>
-
 );
 }
